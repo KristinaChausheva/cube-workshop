8fece672812853e92a8745cb16af1aea774a0c85
Kristina Chausheva <k.chausheva@gmail.com>
2020-06-15 14:47:22 +0300

    connect with the db

--- config/config.js
+++ config/config.js
@@ -1,6 +1,7 @@
 module.exports = {
-    development: {
-        port: process.env.PORT || 3000
-    },
-    production: {}
+  development: {
+    port: process.env.PORT || 3000,
+    databaseUrl: `mongodb+srv://kc1234:${process.env.DB_PASSWORD}@cluster0-1mmle.mongodb.net/softuni?retryWrites=true&w=majority`,
+  },
+  production: {},
 };
--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const env = process.env.NODE_ENV || 'development';
 
+const mongoose = require('mongoose');
 const config = require('./config/config')[env];
 const app = require('express')();
 
 require('./config/express')(app);
 require('./routes')(app);
+
+try {
+  mongoose.connect(
+    config.databaseUrl,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+    () => console.log('connected')
+  );
+} catch (error) {
+  console.log('could not connect');
+}
+// mongoose.connect(
+//   config.databaseUrl,
+//   {
+//     useNewUrlParser: true,
+//   },
+//   (err) => {
+//     console.error(err);
+//     throw err;
+//   }
+// );
 
 app.listen(
   config.port,
